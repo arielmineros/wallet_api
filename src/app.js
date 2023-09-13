94766aebf9b4ee9726684302fa6562bc7345f6b1
Kevin Grande <kevinbussines2020@gmail.com>
2023-09-13 00:44:04 -0600

    ADD: cors to allow frontend app to consume endpoints

--- src/app.js
+++ src/app.js
@@ -1,11 +1,18 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require("cors");
 const authRoutes = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
 const productsRoutes = require("./routes/products.routes");
 //const connectBD = require("./bd");
 
 const app = express();
+app.use(
+    cors({
+      credentials: true,
+      origin: "http://localhost:5173",
+    })
+  );
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());

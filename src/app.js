eb94b42f815a650e7003c492f9d77a7ee6a0361e
arielmineros <arielgmineros@gmail.com>
2023-09-13 01:09:39 -0600

    verification

--- src/app.js
+++ src/app.js
@@ -3,9 +3,11 @@ const morgan = require("morgan");
 const authRoutes = require("./routes/auth.routes");
 const cookieParser = require("cookie-parser");
 const productsRoutes = require("./routes/products.routes");
+const cors = require("cors");
 //const connectBD = require("./bd");
 
 const app = express();
+app.use(cors({ origin: "http://localhost:3000" }));
 app.use(morgan("dev"));
 app.use(express.json());
 app.use(cookieParser());

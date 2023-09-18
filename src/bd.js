14d5a0577dc536ad57c6eaadcf1ab783618b31a5
arielgmineros <bryangmineros@gmail.com>
2023-09-17 22:12:28 -0600

    Adding a new dd connection string

--- src/bd.js
+++ src/bd.js
@@ -3,7 +3,8 @@ const mongoose = require("mongoose");
 const connectBD = async () => {
     try {
         await mongoose.connect(
-            "mongodb+srv://user-api-test:testProject123%40@cluster0.hacdwqy.mongodb.net/Node-API-example?retryWrites=true&w=majority"
+            //"mongodb+srv://user-api-test:testProject123%40@cluster0.hacdwqy.mongodb.net/Node-API-example?retryWrites=true&w=majority"
+            "mongodb+srv://arielgmineros:1234@cluster0.nqoi6ts.mongodb.net/?retryWrites=true&w=majority"
         );
         console.log(">>>> DB is connected");
     } catch (error) {
